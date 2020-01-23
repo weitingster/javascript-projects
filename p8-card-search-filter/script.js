91fcdc34c9f21899845bc3765884e2964d4ae4f4
Weiting <weitingxus@gmail.com>
2020-01-23 12:37:14 -0500

    built the front end for the product cards on p8

---
+++ p8-card-search-filter/script.js
@@ -0,0 +1,6 @@
+/***
+Weiting's 30 days of Vanilla Javascript:
+Project #8: Product Search Filtering
+where user searches for a term on the product page and products will be filtered to
+display on page if it matches the search term.
+***/
