1a5071daf7200ec69ca8bf9957b43525c756234b
Weiting <weitingxus@gmail.com>
2020-01-23 13:55:32 -0500

    removing files for original p8 as project changed

--- p8-card-search-filter/script.js
+++
@@ -1,6 +0,0 @@
-/***
-Weiting's 30 days of Vanilla Javascript:
-Project #8: Product Search Filtering
-where user searches for a term on the product page and products will be filtered to
-display on page if it matches the search term.
-***/
